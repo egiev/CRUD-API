972e5daa99819285c6c8b454c9ae30752176832b
Reginald Mabanta <reginald.ventura@infoshiftinc.com>
2017-07-06 15:42:58 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,19 +1,64 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/app.db')
+
 app = Flask(__name__)
+
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  firstname = db.Column(db.String(80))
+  lastname = db.Column(db.String(80))
 
 
-@app.route('/')
-def index():
-    data = [
-            {'firstname': 'Ryan', 'lastname': 'Ermita', 'position': 'Software Engineer'},
-            {'firstname': 'Reginald', 'lastname': 'Ventura', 'position': 'Frontend Engineer'},
-            {'firstname': 'Chizuru', 'lastname': 'Iwashita', 'position': 'Admin Manager'},
-            {'firstname': 'Lyle', 'lastname': 'Kaiklian', 'position': 'Creative Director'},
-            {'firstname': 'Jezeniel', 'lastname': 'Zapanta', 'position': 'Lead Software Engineer'},
-           ]
+  def to_dict(self):
+    return {
+      "id": self.id,
+      "firstname": self.firstname,
+      "lastname": self.lastname
+    }
 
-    response = {"data": data, "response_code": 200, "message": 'ok'}
+  def __repr__(self):
+    return "<User %r %r %r>" % (self.id, self.firstname, self.lastname)
 
-    return jsonify(response)
+@app.route('/users/', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        users = User.query.all()
+    if request.method == 'POST':
+        data = request.json
+        user = User(**data)
+        db.session.add(user)
+        db.session.commit()
+        users = User.query.all()
+    return jsonify({'data': [u.to_dict() for u in users]})
+
+@app.route('/users/<id>/', methods=['GET', 'DELETE', 'PUT'])
+def user(id):
+    if request.method == 'GET':
+      user = User.query.filter_by(id=id).first().to_dict()
+      return jsonify({ 'data': user})
+
+    if request.method == 'DELETE':
+      user = User.query.filter_by(id=id).first()
+      db.session.delete(user)
+      db.session.commit()
+      users = User.query.all()
+      return jsonify({'data': [u.to_dict() for u in users]})
+
+    if request.method == 'PUT':
+      user = User.query.get(id)
+      user.firstname = request.json.get('firstname')
+      user.lastname = request.json.get('lastname')
+      db.session.commit()
+      users = User.query.all()
+      return jsonify({ 'data': [u.to_dict() for u in users]})
---
+++ schema.sql
@@ -0,0 +1,7 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  firstname text not null,
+  lastname text not null,
+  position text not null
+);
